2b2990fba138e886da26dd14de50aaf7bae4ffe9
edbird <edward.birdsall@postgraduade.manchester.ac.uk>
2020-07-28 21:05:09 +0100

    added files for parallel job launch

---
+++ new/Macros/parallel/parallel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#command="./joblaunch.sh"
+evalcmd="eval $(~/snemo-sdk/bin/brew shellenv)"
+command="root"
+arguments="-b ../newLogLikFitter.C"
+numcores=11
+for i in {0..301}
+do
+
+    sleep 1
+    while [ $( pgrep -c -x "$command" ) -ge "$numcores" ]
+    do
+        kill -SIGSTOP $$
+    done
+
+    (
+        eval $(~/snemo-sdk/bin/brew shellenv)
+        root -b "newLogLikFitter.C($i)"
+        kill -SIGCONT $$
+    ) &
+
+done
