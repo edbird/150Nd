c345428066d6666c6b8774c6c001bf4a2df84840
edbird <edward.birdsall@postgraduade.manchester.ac.uk>
2020-11-01 16:46:00 +0000

    legend

--- new/Macros/newLogLikFitter.h
+++ new/Macros/newLogLikFitter.h
@@ -64,7 +64,7 @@ parameter_group g_pg;
 // fit is done using MinimizeFCNAxialVector
 // the exact fit mode / choice of algorithm is set elsewhere
 //bool g_mode_fit_fake_data = false;
-bool g_mode_fake_data = false;
+bool g_mode_fake_data = true;
 // this might change during program execution
 // for example, it is necessary to set this to true to fit the fake data
 // to measure systematic effects, before switching back to false to fit
